77416f3d51847d11925ac6c367213a517111ebc1
yoshinori91 <yoshinori.negishi91@gmail.com>
2016-11-27 20:29:17 +0900

    update

--- keymap_yoshi.c
+++ keymap_yoshi.c
@@ -1,3 +1,27 @@
+#include "planck.h"
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+
+// Fillers to make layering more clear
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * |   =  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |  ENT |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |   \  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  ~   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | DEL  | Ctrl | Alt  |  ALT | Lower|    Space    | Raise| GUI  |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
 [0] = {
     {KC_EQL,  KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,          KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,           KC_MINS},
     {KC_ENT,  KC_A,    KC_S,    KC_D,    KC_F,         KC_G,          KC_H,    KC_J,         KC_K,    KC_L,    CTL_T(KC_SCLN), KC_QUOT},
