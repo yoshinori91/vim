bdcab143f34f03a855dcd3d79be85675e98dbe30
yoshinori91 <yoshinori.negishi91@gmail.com>
2017-07-25 10:27:59 +0900

    Update keymap_yoshi.c
    update

--- keymap_yoshi.c
+++ keymap_yoshi.c
@@ -11,57 +11,53 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * |   =  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  -   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  ENT |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   \  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  ~   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | DEL  | Ctrl | Alt  |  ALT | Lower|    Space    | Raise| GUI  |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
 [0] = {
-    {KC_EQL,  KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,          KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,           KC_MINS},
-    {KC_ENT,  KC_A,    KC_S,    KC_D,    KC_F,         KC_G,          KC_H,    KC_J,         KC_K,    KC_L,    CTL_T(KC_SCLN), KC_QUOT},
-    {KC_BSLS ,KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,          KC_N,    KC_M,         KC_COMM, KC_DOT,  KC_SLSH,        KC_TILD},
-    {KC_DEL,  KC_LCTL, KC_LALT, KC_LALT, LT(1,KC_ESC), SFT_T(KC_SPC), _______, LT(2,KC_TAB), KC_LGUI, _______, _______,        _______}
+  {KC_EQL,       KC_Q,   KC_W,   KC_E,         KC_R,         KC_T,         KC_Y,   KC_U,         KC_I,         KC_O,   KC_P,         KC_MINS},
+  {LT(3,KC_SCLN),KC_A,   KC_S,   KC_D,         KC_F,         KC_G,         KC_H,   KC_J,         KC_K,         KC_L,   OSM(MOD_LCTL),KC_ENT,},
+  {KC_TILD,      KC_Z,   KC_X,   KC_C,         KC_V,         KC_B,         KC_N,   KC_M,         KC_COMM,      KC_DOT, KC_SLSH,      KC_BSLS},
+  {KC_DEL,       KC_LCTL,KC_LALT,OSM(MOD_LALT),LT(1, KC_ESC),SFT_T(KC_SPC),_______,LT(2, KC_TAB),OSM(MOD_LGUI),KC_DOWN,KC_UP,        KC_RGHT}
 },
-
-/* Lower
- * ,-----------------------------------------------------------------------------------------.
- * |      |      |   &  |   *  |   |  |      |      |   7  |   8  |   9  |             |     |
- * |------+------+------+------+------+------+------+------+------+------+-------------+-----|
- * |      |      |   $  |   %  |   ^  |      |      |   4  |   5  |   6  | Prefix(c-|) |     |
- * |------+------+------+------+------+------+------+------+------+------+-------------+-----|
- * |      |      |   !  |   @  |   #  |      |      |   1  |   2  |   3  |             |     |
- * |------+------+------+------+------+------+------+------+------+------+-------------+-----|
- * |Reset |      |      |      |      |     SPC     |   0  |      |      |             |     |
- * `-----------------------------------------------------------------------------------+-----`
- */
 [1] = {
-    {_______, _______, KC_AMPR, KC_ASTR, KC_PIPE, _______, _______, KC_7, KC_8,    KC_9,    _______,       _______},
-    {_______, _______, KC_DLR,  KC_PERC, KC_CIRC, _______, _______, KC_4, KC_5,    KC_6,    LCTL(KC_PIPE), _______},
-    {_______, _______, KC_EXLM, KC_AT,   KC_HASH, _______, _______, KC_1, KC_2,    KC_3,    _______,       _______},
-    {RESET,   _______, _______, _______, _______, KC_BSPC, _______, KC_0, _______, _______, _______,       _______}
+  {_______,_______,KC_AMPR, KC_ASTR, KC_PIPE, _______, S(KC_QUOT), KC_7, KC_8,    KC_9,    _______,_______},
+  {_______,_______,KC_DLR,  KC_PERC, KC_CIRC, _______, KC_QUOT,    KC_4, KC_5,    KC_6,    KC_APP, _______},
+  {_______,_______,KC_EXLM, KC_AT,   KC_HASH, _______, KC_GRV,     KC_1, KC_2,    KC_3,    _______,_______},
+  {_______,_______,_______, _______, _______, KC_BSPC, _______,    KC_0, _______, _______, _______,_______}
 },
-
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 | F11  | F12  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |   [  |   {  |   (  |      |  ←   |  ↓   |  ↑   |   →  | HOME | END  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |   ]  |   }  |   )  |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |             |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
 [2] = {
-    {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12},
-    {_______, _______, KC_LBRC, KC_LCBR, KC_LPRN, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_HOME, KC_END},
-    {_______, _______, KC_RBRC, KC_RCBR, KC_RPRN, _______, _______, _______, _______, _______,  _______, _______},
-    {_______, _______, _______, _______, _______, _______,  _______, _______, _______, _______,  _______, _______}
+  {KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F12,  KC_F12},
+  {_______, KC_COLN,KC_LBRC, KC_LCBR, KC_LPRN, KC_SCLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_HOME, KC_END},
+  {_______,_______,KC_RBRC, KC_RCBR, KC_RPRN, _______, _______, _______, _______, _______,  KC_PGUP, KC_PGDN},
+  {RESET,  _______,_______, _______, _______, _______, _______, _______, _______, _______,  _______, _______}
 },
+[3] = {
+  {_______, LCAG(KC_Q),    LCAG(KC_W),    LCAG(KC_E),   LCAG(KC_R),    LCAG(KC_T),    LCAG(KC_Y),    LCAG(KC_U),    LCAG(KC_I),    LCAG(KC_O),    LCAG(KC_P),     LCAG(KC_MINS)},
+  {_______, LCAG(KC_A),    LCAG(KC_S),    LCAG(KC_D),   LCAG(KC_F),    LCAG(KC_G),    LCAG(KC_H),    LCAG(KC_J),    LCAG(KC_K),    LCAG(KC_L),    LCAG(KC_SCLN),  LCAG(KC_ENT)},
+  {_______, LCAG(KC_Z),    LCAG(KC_X),    LCAG(KC_C),   LCAG(KC_V),    LCAG(KC_B),    LCAG(KC_N),    LCAG(KC_M),    LCAG(KC_COMM), LCAG(KC_DOT),  LCAG(KC_SLSH),  LCAG(KC_TILD)},
+  {_______, _______,_______, _______, _______, _______, _______, _______, _______, _______,  _______, _______}
+}
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  switch(id) {
+    case 1:
+      if (record->event.pressed) {
+        layer_on(1);
+      } else {
+        layer_off(1);
+      }
+      break;
+    case 2:
+      if (record->event.pressed) {
+        layer_on(2);
+      } else {
+        layer_off(2);
+      }
+      break;
+  }
+  return MACRO_NONE;
 };
