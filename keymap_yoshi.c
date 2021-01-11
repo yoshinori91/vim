773aa688534cfd3abefa961250accd1f67913748
yoshinori91 <yoshinori.negishi91@gmail.com>
2021-01-12 02:06:16 +0900

    Update keymap_yoshi.c
    update planck

--- keymap_yoshi.c
+++ keymap_yoshi.c
@@ -57,6 +57,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+[0] = LAYOUT_planck_grid(
+  KC_EQL,  KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,    KC_Y,          KC_U,         KC_I,    KC_O,    KC_P,    KC_MINS,
+  KC_SCLN, KC_A,    KC_S,    KC_D,    KC_F,         KC_G,    KC_H,          KC_J,         KC_K,    KC_L,    OSM(MOD_LCTL), KC_ENT,
+  KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,    KC_N,          KC_M,         KC_COMM, KC_DOT,  KC_SLSH, A(KC_GRAVE),
+  _______, _______, _______, KC_LALT, LT(1,KC_ESC), _______, SFT_T(KC_SPC), LT(2,KC_TAB), KC_LGUI, _______, _______, _______
+),
+
+[1] = LAYOUT_planck_grid(
+  _______, _______, S(KC_7), S(KC_8), S(KC_9), _______, _______,   KC_7, KC_8,    KC_9,    _______, _______,
+  _______, _______, S(KC_4), S(KC_5), S(KC_6), _______, S(KC_QUOT),KC_4, KC_5,    KC_6,    KC_QUOT, _______,
+  _______, _______, S(KC_1), S(KC_2), S(KC_3), _______, _______,   KC_1, KC_2,    KC_3,    _______, _______,
+  RESET,   _______, _______, _______, _______, _______, KC_BSPC,   KC_0, _______, _______, _______, _______
+),
+
+[2] = LAYOUT_planck_grid(
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,
+  _______, _______, _______, KC_LBRC, S(KC_9),    S(KC_LBRC), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_HOME, KC_END,
+  _______, _______, _______, KC_RBRC, S(KC_0),   S(KC_RBRC), _______, _______, _______, _______,  _______, _______,
+  RESET,   _______, _______, _______, A(KC_GRAVE), _______,   _______, _______, _______, _______,  _______, _______
+),
+};
+
+
 qk_tap_dance_action_t tap_dance_actions[] = {
   [SFT_CAP] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS)
 };
