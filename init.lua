11762f60471bde7e634e7b0c5e27bb7a6ca2253b
yoshinori91 <yoshinori.negishi91@gmail.com>
2023-06-09 17:59:23 +0900

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,12 @@
+require 'extensions'
+
+vim.cmd[[colorscheme tokyonight]]
+
+vim.opt.number = true
+vim.opt.list = true
+vim.opt.listchars = {eol='⤶',tab='➤·',trail='×',nbsp='⎵'}
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
